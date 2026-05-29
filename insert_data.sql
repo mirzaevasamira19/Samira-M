f0d7c2589e6b7a28403d2f16203e908102b28f21
mirzaevasamira19 <mirzaevasamira74@gmail.com>
2026-05-29 20:13:16 +0500

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,27 @@
+INSERT INTO Developers VALUES
+(1,'Valve','USA','valve.com'),
+(2,'CD Projekt Red','Poland','cdprojektred.com');
+
+INSERT INTO Users VALUES
+(1,'samira','samira@mail.com','12345','2025-01-01'),
+(2,'aziz','aziz@mail.com','12345','2025-01-02');
+
+INSERT INTO Games VALUES
+(1,'Half-Life 2','Shooter','2004-11-16',9.99,1),
+(2,'The Witcher 3','RPG','2015-05-19',29.99,2);
+
+INSERT INTO Genres VALUES
+(1,'Shooter'),
+(2,'RPG');
+
+INSERT INTO Game_Genres VALUES
+(1,1),
+(2,2);
+
+INSERT INTO Libraries VALUES
+(1,1,1,'2025-01-10',50),
+(2,2,2,'2025-01-11',100);
+
+INSERT INTO Reviews VALUES
+(1,1,1,10,'Excellent game','2025-01-12'),
+(2,2,2,9,'Great RPG','2025-01-13');
